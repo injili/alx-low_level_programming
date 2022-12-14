a0d9b4db1a76a9e617fa833974dd2f992848684b
injili <nyarekigospel@gmail.com>
2022-12-14 12:30:00 -0800

    .

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+void capital(char d);
+
+int _isalpha(int c)
+{
+	char y = c;
+	char q;
+	char w;
+
+	for (q = 'a'; a <= 'z'; ++a)
+	{
+		if (q == y)
+		{
+			return (1);
+			break;
+		}
+		else if (q == 'z')
+		{
+			capital(y);
+			break;
+		}
+		else
+		{
+			continue;
+		}
+	}
+}
+
+void capital(char d)
+{
+	for (w = 'A' ; w <= 'Z'; ++w)
+	{
+		if (d == w)
+		{
+			return (1);
+			break;
+		}
+		else if (w == 'Z')
+		{
+			return (0);
+			break;
+		}
+		else
+		{
+			continue;
+		}
+	}
+}
