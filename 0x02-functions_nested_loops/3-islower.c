ae9fcb3008df5f3c2d698801559812f7c32b707a
injili <nyarekigospel@gmail.com>
2022-12-14 09:24:36 -0800

    .

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int _islower(int c)
+{
+	char a = c;
+	char i;
+	for (i = 'a'; i <= 'z'; ++i)
+	{
+		if (i == a)
+		{
+			return (1);
+			break;
+		}
+		else if (i == 'z')
+		{
+			return (0);
+		}
+		else
+		{
+			continue;
+		}
+			
+	}
