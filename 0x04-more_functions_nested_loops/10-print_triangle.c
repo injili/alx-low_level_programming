46d016478eba12502d978d3f85a91d1cb636ac06
injili <nyarekigospel@gmail.com>
2022-12-15 15:30:18 -0800

    .

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,22 +1,40 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * print_triangle - draws a triangle
+ * @size: how big the triangle gon be
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-void print_triangle(int size);
+void print_triangle(int size)
 {
 	if (size > 0)
 	{
 		int a;
-		int b;
+		int b = size - 1;
+		int d;
+		int c;
+		int e;
 
 		for (a = 0; a < size; a++)
 		{
-			for (b = 1; b <= size; b==)
+			for (d = 0; d < b; d++)
 			{
-
+				_putchar(' ');
+			}
+			c = size - b;
+			for (e = 0; e < c; e++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			b--;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
