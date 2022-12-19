32e2cbbd16ff82b58fff43bc9dfef539901ef4d3
injili <nyarekigospel@gmail.com>
2022-12-19 06:52:31 -0800

    .

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value in n to 98
+ * @n:receives the Address to n
+ *
+ * Return: On success 0.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
