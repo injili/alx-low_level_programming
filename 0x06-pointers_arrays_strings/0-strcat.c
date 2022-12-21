a7e73e7271b16a62fa65f165686f35e12c965a25
injili <nyarekigospel@gmail.com>
2022-12-21 12:02:12 -0800

    .

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcat - the function concatenates two strings
+ * @dest: the destination file
+ * @src: the second file
+ *
+ * Return: the pointer to the destination file
+ */
+
+char *_strcat(char *dest, char *src)
+{
+}
