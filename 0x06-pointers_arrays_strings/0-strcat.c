f4d5a0d1d19841843f1248d88b0ca83204aa9fb8
injili <nyarekigospel@gmail.com>
2022-12-21 12:22:52 -0800

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,4 +10,26 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int len;
+	int i;
+	int j = 0;
+
+	for (len = 1 ; ; len++)
+	{
+		if (*(dest + len) == 0)
+			break;
+	}
+
+	i = len;
+
+	while (*src)
+	{
+		*(dest + len) = *(src + j);
+		j++;
+		len++;
+	}
+
+	*(dest + len) = '\0';
+
+	return (dest);
 }
