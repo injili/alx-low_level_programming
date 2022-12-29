916a2cf150257c92de410eb0625fc18603fb73b1
injili <nyarekigospel@gmail.com>
2022-12-29 11:07:20 -0800

    Low-level Programming

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * rot13 - does encoding using rot13
+ * @c: pointer to the string that is to be encoded
+ *
+ * Return: the encoded string
+ */
+
+char *rot13(char *c)
+{
+	int a;
+	return (c);
+}
