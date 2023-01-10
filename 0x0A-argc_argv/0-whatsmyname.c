ac9808274842c39d71803712c317aa937800301b
injili <nyarekigospel@gmail.com>
2023-01-10 01:20:21 -0800

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function prints out the program name
+ *
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[1]);
+	return (0);
+}
