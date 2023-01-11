a558e665d540d21410526a8d81efb7a572e21732
injili <nyarekigospel@gmail.com>
2023-01-11 13:29:33 -0800

    2D greed :)

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,13 +4,22 @@
 /**
  * alloc_grid - returns a pointer to a @D array of integers
  * @width: the width of the grid
- * #height: the height of the grid
+ * @height: the height of the grid
  *
  * Return: pointer s to the grid
  */
 
 int **alloc_grid(int width, int height)
 {
-	int *s;
+	int **s, i, j;
 
+	s = malloc(sizeof(int **) * height);
+	for (i = 0; i < height; i++)
+		s[i] = malloc(height * sizeof(int *));
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			s[i][j] = 0;
+	}
+	return (s);
 }
