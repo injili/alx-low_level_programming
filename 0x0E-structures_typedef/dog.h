c8e65cf0f734851542d0aa5ac1276228357f467d
injili <nyarekigospel@gmail.com>
2023-01-17 16:34:38 -0800

    dogs and they loyalty :)

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - details on a dog
+ * @name: the dog's name
+ * @age: the age of the dog
+ * @owner: who does the dog belong to?
+ *
+ * Description: A structure that is used for dog details
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
