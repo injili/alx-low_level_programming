25b4de9e9d526be5d5d986d17593a30d2ff33880
injili <nyarekigospel@gmail.com>
2023-01-17 18:47:15 -0800

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name:  the new dog name
+ * @age: the new dog age
+ * @owner: who owns the new dog?
+ * 
+ * Return: Null if function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t.name = name;
+	dog_t.age = age;
+	dog_t.owner = owner;
+}
