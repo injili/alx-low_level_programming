f27007236d9471771b4eb8035e4459b74c7eb69f
injili <nyarekigospel@gmail.com>
2023-01-23 11:08:38 -0800

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
 /**
+ * _puts_recursion - print a string
+ * @s: pointer block of memory to fill
+ *
+ * Return: void
  */
 
 void _puts_recursion(char *s)
 {
-	if (*(s + i) == '\0')
-		return (s);
-	i++;
-	return ();
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
