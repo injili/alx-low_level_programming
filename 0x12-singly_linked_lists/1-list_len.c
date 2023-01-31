57b2b8f218760140208dc1be85837e68c2d09214
injili <nyarekigospel@gmail.com>
2023-01-30 17:02:15 -0800

    count em all

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,23 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
 
+/**
+ * list_len - returns number of elements in a linked list
+ * @h: pointer to the head of the linked list
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
