9c9864db966646f58477c30e366918cfaa81f8e4
injili <nyarekigospel@gmail.com>
2023-01-30 16:43:25 -0800

    print all elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,14 +1,29 @@
+#include <stdlib>
+#include <stdio.h>
+#include <stddef.h>
+#include "list.h"
+
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * print_list - function to print out all the elements of a list
+ * @h: pointer to the head of the linked list
  *
- * Description: singly linked list node structure
+ * Return: the number of nodes
  */
-typedef struct list_s
+
+size_t print_list(const list_t *h)
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
