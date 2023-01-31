8a8220d1dc4a175b39aee0c65a158910ab60a00a
injili <nyarekigospel@gmail.com>
2023-01-30 17:50:51 -0800

    add a node to the end of the linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,14 +1,43 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include "lists.h"
+
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * add_node_end - adds an element to the end of a linked list
+ * @head: pointer to the head of the linked list
+ * @str:string passed to the function
  *
- * Description: singly linked list node structure
+ * Return: address of the new element or NULL if it failed
  */
-typedef struct list_s
+
+list_t *add_node_end(list_t **head, const char *str)
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+	list_t *new_node, *current_node;
+	size_t count;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	for (count = 0; str[count]; count++)
+		;
+
+	new_node->len = count;
+	new_node->next = NULL;
+	current_node = *head;
+
+	if (current_node == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		while (current_node->next != NULL)
+			current_node = current_node->next;
+		current_node->next = new_node;
+	}
+
+	return (*head);
+}
