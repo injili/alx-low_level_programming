b6bb496dea720b47819233f18b041e32079fc709
injili <nyarekigospel@gmail.com>
2023-02-02 13:55:33 -0800

    clear to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,7 +13,7 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 
 	if (head == NULL)
-		return (NULL);
+		return;
 
 	while (*head)
 	{
@@ -21,5 +21,6 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = temp;
 	}
+
 	head = NULL;
 }
