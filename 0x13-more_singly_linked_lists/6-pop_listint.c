4654ffe8b21e7b4e36a7d1af651a6cdae6796f9b
injili <nyarekigospel@gmail.com>
2023-02-02 14:19:21 -0800

    linked list freeing

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,3 +7,19 @@
  *
  * Return: the head node's data
  */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	n = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (n);
+}
