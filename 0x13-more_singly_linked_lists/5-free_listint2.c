ecff1fc6b08a5ebf2ec9de6c1ece84e3f0f80a7e
injili <nyarekigospel@gmail.com>
2023-02-02 13:29:55 -0800

    free up all heap blocks

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list and sets the head to NULL
+ * @head: pointer to the head of the linked list
+ *
+ * Return: no return value
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *temporary;
+
+	temp = *head;
+	while (head != NULL)
+	{
+		temporary = temp;
+		temporary = temp->next;
+		free(temp);
+	}
+	*head = NULL;
+}
