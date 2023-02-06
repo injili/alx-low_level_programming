5f76961064a29dd9eacf65c6617a0ee99014b94b
injili <nyarekigospel@gmail.com>
2023-02-06 15:39:31 -0800

    print out in binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_binary - prints out a decimal number in binary format
+ * @n: the intger passed to the function
+ *
+ * Return: no return value
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
