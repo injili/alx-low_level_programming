f87d49b8df677cb1be1075d160ff38490a03c06a
injili <nyarekigospel@gmail.com>
2023-02-06 17:40:38 -0800

    get value at index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,4 +11,14 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int x;
+
+	if (n == 0 && index < 64)
+		return (0);
+
+	for (x = 0; index < 64; n >>= 1, x++)
+	{
+		if (index == x)
+			return (n & 1);
+	}
+	return (-1);
 }
