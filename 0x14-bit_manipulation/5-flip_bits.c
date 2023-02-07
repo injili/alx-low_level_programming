b9a50be8d5b2dbbf9f0cf99c7074354c5b99213e
injili <nyarekigospel@gmail.com>
2023-02-07 06:54:02 -0800

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns umber f bits you would need
+ * to flip to get from one whole number to another
+ * @n: the first integer
+ * @m: the second integer
+ *
+ * Return: the count number
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+}
