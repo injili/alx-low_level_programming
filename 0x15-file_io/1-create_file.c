142803a562fd264eb48da846e5db903e00fe619c
injili <nyarekigospel@gmail.com>
2023-02-10 03:20:07 -0800

    create a file using function descriptors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,7 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "main.h"
-
 /**
  * create_file - creates a file
  * @filename: the name of the file to create
@@ -10,8 +12,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	if (filename == NULL)
+	int fd, letters, rw;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
 		return (-1);
 
-	return ();
+	if (!text_content)
+		text_content = "";
+
+	for (letters = 0; text_content[letters]; letters++)
+		;
+
+	rw = write(fd, text_content, letters);
+
+	if (rw == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
