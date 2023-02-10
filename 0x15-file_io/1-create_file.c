ffce60c5959f00f7b6227db08876cf2c9f16bdd2
injili <nyarekigospel@gmail.com>
2023-02-10 02:32:24 -0800

    create a file using file descriptors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file to create
+ * @text_content: NULL terminated string to write ti the file
+ *
+ * Return: 1on success and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	if (filename == NULL)
+		return (-1);
+
+	return ();
+}
