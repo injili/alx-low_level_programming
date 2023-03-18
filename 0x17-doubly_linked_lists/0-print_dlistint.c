3a68967ebc889df322c517a2b5df1991893b4507
injili <nyarekigospel@gmail.com>
2023-03-18 10:31:17 -0700

    resque

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @dlistint: the list passed to the function
+ * @h: pointer to the head of the linked list
+ *
+ * Return: 0 for succes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+
+}
