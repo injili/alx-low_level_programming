c2355aaab9cfc1b37a9fb2e98d7739d009555572
injili <nyarekigospel@gmail.com>
2023-03-21 08:15:19 -0700

    add new node to the beginning

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,6 +18,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
