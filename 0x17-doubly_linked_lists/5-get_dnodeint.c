83fc50a5e6354e78ddad77e426d77e48af82b9e1
injili <nyarekigospel@gmail.com>
2023-03-21 11:37:29 -0700

    get a node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -21,6 +21,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		if (index == count)
 		{
+			if (get_node == NULL)
+				return (NULL);
 			get_node->n = head->n;
 			get_node->prev = head->prev;
 			get_node->next = head->next;
