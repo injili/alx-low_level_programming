a4dbb027e6eb0cc2e63c388db82864ca77812860
injili <nyarekigospel@gmail.com>
2023-03-20 22:17:44 -0700

    print list elements

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,6 @@
 
 /**
  * print_dlistint - prints all the elements of a dlistint_t list
- * @dlistint: the list passed to the function
  * @h: pointer to the head of the linked list
  *
  * Return: 0 for succes
@@ -10,5 +9,14 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t count;
 
+	count = 0;
+	while (h != NULL)
+	{
+		printf("[%d]", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
