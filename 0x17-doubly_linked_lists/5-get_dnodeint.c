2a9153b9bbcedba0e46143e9ef66d8f355da63e0
injili <nyarekigospel@gmail.com>
2023-03-21 11:29:14 -0700

    get a node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - gets the element at a certain node
+ * @head: head to the first node in the linked list
+ * @index: The index to be looked for
+ *
+ * Return: the node in search
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count;
+	dlistint_t *get_node;
+
+	count = 0;
+	get_node = malloc(sizeof(dlistint_t));
+	while (head != NULL)
+	{
+		if (index == count)
+		{
+			get_node->n = head->n;
+			get_node->prev = head->prev;
+			get_node->next = head->next;
+			break;
+		}
+		else
+		{
+			head = head->next;
+			count++;
+		}
+	}
+	return (get_node);
+}
