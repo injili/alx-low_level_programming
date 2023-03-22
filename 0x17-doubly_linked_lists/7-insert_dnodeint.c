cd6af631694c3549ff2df14f05d2e0f6595e9c21
injili <nyarekigospel@gmail.com>
2023-03-22 02:57:35 -0700

    add nde at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,6 +25,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = NULL;
 	new_node->next = NULL;
 
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+
+		*h = new_node;
+		return (new_node);
+	}
+
 	while (*h != NULL)
 	{
 		if (count == idx)
