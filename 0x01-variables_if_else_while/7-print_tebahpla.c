2c7aaa00a4b0e59f854df81e15633df2804dec63
injili <nyarekigospel@gmail.com>
2023-05-05 12:10:01 -0700

    the alphabet backwards

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - the main function to the program
+ * @void: function main is receiving no arguments
+ *
+ * Return: 0 on successful execution
+ */
+
+int main(void)
 {
 	int i;
+
 	for (i = 'z'; i >= 'a'; i--)
 	{
 		putchar(i);
