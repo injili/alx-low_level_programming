57bfa63d9c5d720fa6cb55da06abf7211d1032b6
injili <nyarekigospel@gmail.com>
2023-05-04 22:34:19 -0700

    lowercase alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - the main function of the program
+ * @void: main function receives no arguments
+ *
+ * Return: 0 on successful execution
+ *
+ */
+int main(void)
 {
 	int i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
