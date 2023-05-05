97370ff0b362e411652eec7c9e238d38b16471c7
injili <nyarekigospel@gmail.com>
2023-05-04 23:49:42 -0700

    alphabet without q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - the main function of the program
+ * @void: the function receives no function arguments
+ *
+ * Return: 0 on succesful running of the code
+ */
+int main(void)
 {
 	int i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		if (i == 'p' | i == 'e')
+		if (i == 'q' || i == 'e')
 			continue;
 		putchar(i);
 	}
+
 	putchar('\n');
+
 	return (0);
 }
