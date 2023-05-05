877f7fdc1d7dbd922c87b03a14e44c8ae9d9a003
injili <nyarekigospel@gmail.com>
2023-05-05 00:06:26 -0700

    print out numbers without the use of the data type char

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
-
-int main(long i)
+/**
+ * main - main function of the program
+ * void: function main receives np arguments
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
 {
+	int long i;
+
 	for (i = 0; i < 10; i++)
 	{
-		putchar((i%10) + '0');
+		putchar((i % 10) + '0');
 	}
+
 	putchar('\n');
+
 	return (0);
 }
