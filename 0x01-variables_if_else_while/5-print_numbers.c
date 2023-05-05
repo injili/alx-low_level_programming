09e03e09533130cb5517347065e142bfaba06d0a
injili <nyarekigospel@gmail.com>
2023-05-04 23:56:36 -0700

    single digit numbers of base 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - the main and only function of the program
+ * @void: the main function receives no argument
+ *
+ * Return: 0 on successful execution
+ */
+
+int main(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
-		printf("%d",i);
+		printf("%d", i);
 	}
 	printf("\n");
 	return (0);
