2db0f0751b4efa023c8e1500dfc24437ec0fbc58
injili <nyarekigospel@gmail.com>
2023-05-06 12:17:40 -0700

    find the sign of the number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,27 @@
 #include "main.h"
 
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number passed to the function
+ *
+ * Return: 0 on successful execution
+ */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
-		return 1;
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		return 0;
+		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		return -1;
+		return (-1);
 	}
 }
