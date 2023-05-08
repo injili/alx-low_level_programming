bd723413e215b16ff3fc6a3fad248e928bdb1ed2
injili <nyarekigospel@gmail.com>
2023-05-08 10:10:56 -0700

    count numbers up to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,75 @@
 #include "main.h"
 
 /**
+ * greater_than_98 - does the enumeration for numbers greater than 98
+ * @n: the integer passed to the function
+ *
+ * Return: there is no return value
  */
 void greater_than_98(int n)
 {
+	for (; n > 97; n--)
+	{
+		if (n < 100)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		else
+		{
+			_putchar((n / 100) + '0');
+			_putchar(((n / 10) % 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
 }
 /**
+ * print_to_98 - prints all numbers from n to 98
+ * @n: the integer value passed to the function
+ *
+ * Return: there is no return value
  */
 void print_to_98(int n)
 {
+	int a;
+
+	if (n < 99)
+	{
+		for (; n < 99; n++)
+		{
+			if (n < 0)
+			{
+				_putchar('-');
+				a = -n;
+			}
+			else
+			{
+				a = n;
+			}
+			if (a < 10)
+			{
+				_putchar(a + '0');
+			}
+			else
+			{
+				_putchar((a / 10) + '0');
+				_putchar((a % 10) + '0');
+			}
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		greater_than_98(n);
+	}
+	_putchar('\n');
 }
