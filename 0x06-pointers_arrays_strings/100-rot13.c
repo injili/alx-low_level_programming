5c482db7b17526a4e3af64230acb60d9e20a6e4e
injili <nyarekigospel@gmail.com>
2023-05-10 05:32:03 -0700

    rot13 encoding

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,14 +9,20 @@
 
 char *rot13(char *c)
 {
-	int i;
+	int j, i;
+	char alpha[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char rot13[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
 
 	for (i = 0; c[i] != '\0'; i++)
 	{
-		if ((c[i] >= 'A' && c[i] < 'N') || (c[i] >= 'a'  && c[i] < 'n'))
-			c[i] += 13;
-		else if ((c[i] > 'M' && c[i] <= 'Z') ||	(c[i] > 'm' && c[i] <= 'z'))
-			c[i] -= 13;
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (c[i] == alpha[j])
+			{
+				c[i] = rot13[j];
+				break;
+			}
+		}
 	}
 	return (c);
 }
