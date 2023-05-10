7872156d9dbf94a2b7f1ef722b4ccc113e0a25c2
injili <nyarekigospel@gmail.com>
2023-05-10 08:00:15 -0700

    search

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,10 +10,12 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j, k = 0;
+	int i, j;
 
 	for (i = 0; *(s + i) != '\0' ; i++)
 	{
+		int k = 0;
+
 		for (j = 0; *(accept + j) != '\0' ; j++)
 		{
 			if (*(s + i) == *(accept + j))
