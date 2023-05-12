7477066f6b54f4b48f3f11abbc9dc26b24eb80d3
injili <nyarekigospel@gmail.com>
2023-05-12 02:03:27 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_chessboard - A function that prints the chessboard.
+ * @a: The string passed to the function.
+ *
+ * Return: No return value.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int 8;
+}
+
