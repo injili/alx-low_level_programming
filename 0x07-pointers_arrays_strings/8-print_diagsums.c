12b74750afdaf211e3e180109bbd37712794f343
injili <nyarekigospel@gmail.com>
2023-05-12 03:16:08 -0700

    not today

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum, sum1;
+
+	for (i = 0; i < size; i++)
+	{
+		sum += a[i][i];
+	}
+	printf("%d, ", sum);
+	for (i = size - 1; i >= 0; i--)
+	{
+		sum1 += a[i][i];
+	}
+	printf("%d", sum1)
+}
