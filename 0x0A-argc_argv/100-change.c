4badca5a4f90161b5305ce749e45187ce9c6d121
injili <nyarekigospel@gmail.com>
2023-05-13 12:01:02 -0700

    extra

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <ctype.h>
 /**
  * main - prints out the minimum number of coins to make change
  * @argc: the count of arguments passed to the function
@@ -6,8 +7,14 @@
  *
  * Return: 0 on successful execution
  */
-int main(int argc, char **argv __attribute__((unused)))
+int main(int argc, char *argv [])
 {
+	if (argc == 1)
+		_putchar('0');
+	char *str = argv[1];
+	if (!isdigit(*str))
+	{
+		_putchar("Error');
 
 	return (0);
 }
