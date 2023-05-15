41a61033780e40864915b4f1003e95c76d427a75
injili <nyarekigospel@gmail.com>
2023-05-15 07:16:31 -0700

    2 dimension array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,18 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
+	{
 		s[i] = malloc(width * sizeof(int));
+		if (s[i] == NULL)
+		{
+			for (j = i; j >= 0; j++)
+			{
+				free(s[j]);
+			}
+			free(s);
+			return (NULL);
+		}
+	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
