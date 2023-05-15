cea4a885595e75c19e8d5267b02ef5b4384d443b
injili <nyarekigospel@gmail.com>
2023-05-15 02:26:25 -0700

    malloc creation of new list

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,8 +17,7 @@ char *create_array(unsigned int size, char c)
 	s = malloc(sizeof(char) * size);
 	if (s == NULL)
 	{
-		s[0] = '\0';
-		return (s);
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 		s[i] = c;
