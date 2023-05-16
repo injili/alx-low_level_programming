0f59f00531d2e21a1b9b25404c4b7b2c55346f32
injili <nyarekigospel@gmail.com>
2023-05-16 11:49:21 -0700

    calloc usage to allocate memory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,15 +1,38 @@
+/*
+ *       (_)
+ *  \____/..
+ *   (___/\/
+ *   || ||
+ *
+ *   injili
+ */
+
 #include "main.h"
 #include <stdlib.h>
 
 /**
  * _calloc - allocates memory for an array
  * @nmemb: the array passed to the function
- * @size: the bytes 
+ * @size: the bytes
  *
  * Return: pointer to the memory allocated
  */
-
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	return (s);
+	void *mem;
+	char *fill;
+	unsigned int index;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+
+	mem = malloc(size * nmemb);
+	if (mem == NULL)
+		return (NULL);
+
+	fill = mem;
+	for (index = 0; index < nmemb * size; index++)
+		fill[index] = '\0';
+
+	return (mem);
 }
