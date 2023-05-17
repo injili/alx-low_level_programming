5d48f87467e3eb2c51270876d45806a3d51eb45b
injili <nyarekigospel@gmail.com>
2023-05-17 04:42:32 -0700

    print out the dog,s information

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,18 +15,18 @@ void print_dog(struct dog *d)
 		if (d->name)
 			printf("%s\n", d->name);
 		else
-			printf("(nil)");
+			printf("(nil)\n");
 
 		printf("Age: ");
 		if (d->age)
 			printf("%f\n", d->age);
 		else
-			printf("(nil)");
+			printf("(nil)\n");
 
 		printf("Owner: ");
 		if (d->owner)
 			printf("%s\n", d->owner);
 		else
-			printf("(nil)");
+			printf("(nil)\n");
 	}
 }
